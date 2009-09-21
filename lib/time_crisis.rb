ff257e6be6d258ddabde63a3c0434207aacc7750
Travis Tilley <ttilley@gmail.com>
2009-09-20 20:45:51 -0400

    don't load time_crisis/support if we're using active support.

--- lib/time_crisis.rb
+++ lib/time_crisis.rb
@@ -1,6 +1,10 @@
 module TimeCrisis; end
 
-require 'time_crisis/support'
+# the support directory is a simplified version of the same methods in
+# ActiveSupport, and are completely unnecessary if we're actually
+# using that library. I just didn't want to depend on it.
+require 'time_crisis/support' unless defined?(ActiveSupport)
+
 require 'time_crisis/meteorological_seasons'
 require 'time_crisis/date_range'
 require 'time_crisis/named_months'
