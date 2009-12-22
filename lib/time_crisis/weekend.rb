491fe5cba413d8042f1bae76a01cdad731692292
Travis Tilley <ttilley@gmail.com>
2009-12-22 13:54:20 -0500

    weekday? -> !weekend?

--- lib/time_crisis/weekend.rb
+++ lib/time_crisis/weekend.rb
@@ -2,6 +2,10 @@ module TimeCrisis::Weekend
   def weekend?
     [6, 7].include? self.cwday
   end
+
+  def weekday?
+    !weekend?
+  end
 end
 
 ::TimeCrisis::Date.send(:include, TimeCrisis::Weekend)
