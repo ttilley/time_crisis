577e00b2013cf04dac6f15525885bcb8301a3222
Travis Tilley <ttilley@gmail.com>
2009-12-22 12:41:19 -0500

    fix some oddities with ThirdBase code being missing from TimeCrisis in ruby 1.9, added some ugly initial code for converting time zones in TimeCrisis::DateTime

--- lib/time_crisis.rb
+++ lib/time_crisis.rb
@@ -7,9 +7,7 @@ end
 
 require 'third_base'
 
-module TimeCrisis
-  include ThirdBase
-end
+TimeCrisis = ThirdBase.dup
 
 # add needed bits of active support to time crisis
 # necessary now that it uses ThirdBase
@@ -21,3 +19,22 @@ require 'time_crisis/named_months'
 require 'time_crisis/weekend'
 require 'time_crisis/nth_weekday'
 require 'time_crisis/holiday'
+
+# TODO: make this less of an ugly hack
+module TimeCrisis
+  class DateTime
+    ENVMUTEX = Mutex.new
+    def for_zone(time_zone)
+      ENVMUTEX.synchronize do
+        original_zone = ENV['TZ']
+        begin
+          utc_time = self.to_time.utc
+          ENV['TZ'] = time_zone
+          utc_time.localtime.to_tc_datetime
+        ensure
+          ENV['TZ'] = original_zone
+        end
+      end
+    end
+  end
+end
