d665bbef4f4976d303d3add7472f8ea409827d5a
Travis Tilley <ttilley@gmail.com>
2009-12-31 02:37:46 -0500

    extend the right DateTime ^^;

--- lib/time_crisis/support/ext/date_time/zones.rb
+++ lib/time_crisis/support/ext/date_time/zones.rb
@@ -1,5 +1,7 @@
-class DateTime
-  def in_time_zone(zone = ::TimeCrisis::Time.zone)
-    TimeCrisis::TimeWithZone.new(utc? ? self : getutc, ::TimeCrisis::Time.__send__(:get_zone, zone))
+module TimeCrisis
+  class DateTime
+    def in_time_zone(zone = ::TimeCrisis::Time.zone)
+      TimeCrisis::TimeWithZone.new(utc? ? self : getutc, ::TimeCrisis::Time.__send__(:get_zone, zone))
+    end
   end
 end
